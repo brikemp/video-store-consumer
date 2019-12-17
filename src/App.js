2125fdb796968d6beca38c910456baf249be378f
Brianna Kemp <kemp.bri@gmail.com>
2019-12-17 13:56:43 -0800

    Created Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Search from './components/Search'
+import CustomerList from './components/CustomerList'
+import Library from './components/Library'
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,67 +10,49 @@ import {
   Link
 } from "react-router-dom";
 
+
 function Home() {
   return <h2>Home</h2>;
 }
 
-function Search() {
-  return <h2>Search</h2>;
-}
-
-function Library() {
-  return <h2>Library</h2>;
-}
-
-function Customers() {
-  return <h2>Customers</h2>;
-}
-
 class App extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
 
-    
-    return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/search">Search</Link>
-            </li>
-            <li>
-              <Link to="/library">Library</Link>
-            </li>            
-            <li>
-              <Link to="/customers">Customers</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/customers">
-            <Customers />
-          </Route>          
-          <Route path="/search">
-            <Search />
-          </Route>
-          <Route path="/library">
-            <Library />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-    );
+    }
   }
 
+  render () {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/search">Search</Link>
+              </li>
+              <li>
+                <Link to="/library">Library</Link>
+              </li>            
+              <li>
+                <Link to="/customers">Customers</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/search" component={Search} />
+            <Route path="/library" component={Library} />
+            <Route path="/customers" component={CustomerList} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
