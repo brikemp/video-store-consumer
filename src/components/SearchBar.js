9e604f8369b0400a36f5f6e5b0481a68ec84988a
Brianna Kemp <kemp.bri@gmail.com>
2019-12-17 16:07:39 -0800

    Non-functioning search bar added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,50 +5,72 @@ import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './SearchBar.css';
 
-const SearchBar = (props) => {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      query: "",
+      query: '', 
       queryResults: [],
       error: '', 
+      search_check: false, 
     }
   }
 
-  searchMovies = () => {
-    axios.get('http://localhost:3000/movies?query=' + this.state.query)
+  onInputChange = (event) => {
+    const updatedState = {};
+
+    const field = event.target.name;
+    const value = event.target.value;
+
+    updatedState[field] = value;
+    this.setState(updatedState);
+  }
+
+
+  searchMovies = (event) => {
+    console.log(event);
+    axios.get('http://localhost:3000/movies/', { params: { query: this.state.query } } )
       .then(response => {
         const movies = response.data.map(movie => {
           return movie;
         });
         this.setState({
           queryResults: movies,
+          search_check: true
         });
       })
       .catch((error) => {
-        this.setState({ error: "sdgfhgjghfdz" })
+        this.setState({ error: "No movies found" })
       })
   };
 
-  return (
-    <section>
-      <div>
-        <label className="search-bar--label" htmlFor="searchBar">Search</label>
-      </div>
-      <input
-        onChange={(event) => { searchChangeCallback(event.target.value) }}
-        value={searchTerm}
-        name="searchBar"
-        id="searchBar"
-        className="search-bar"
-      />
-    </section>
-  );
-};
 
-SearchBar.propTypes = {
-  searchChangeCallback: PropTypes.func.isRequired,
-  searchTerm: PropTypes.string.isRequired,
+  render () {
+    return (
+      <div>
+        <form onSubmit={this.searchMovies}>
+          <h3>Search for a Movie</h3>
+          <div>
+            <label htmlFor="query">Movie Title:</label>
+            <input
+              type='text'
+              onChange={this.onInputChange}
+              value={this.state.query}
+              name="query"
+              id="query"
+              className="search-bar"
+            />
+          </div>
+          <button type="submit">Search</button>
+        </form>
+        <div>
+          <h2>{this.state.searchCheck ? "Results" : '' }</h2>
+          {this.state.queryResults}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default SearchBar;
