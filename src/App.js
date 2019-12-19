f55da1d4ef0344830ed20ad3e3573fc824a1fa34
Brianna Kemp <kemp.bri@gmail.com>
2019-12-19 12:14:22 -0800

    Changed logo style to CSS instead of inline

--- src/App.js
+++ src/App.js
@@ -122,7 +122,7 @@ onSelectCustomer = (customerId) => {
           <div>
             <nav className="navbar navbar-width">
                 <a class="navbar-brand" href="/">
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Blockbuster_logo.svg/2000px-Blockbuster_logo.svg.png" width="30" height="30" alt="blockbuster logo" />
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Blockbuster_logo.svg/2000px-Blockbuster_logo.svg.png" className="logo-size" alt="blockbuster logo" />
                 </a>
                 <Link className="nav-link" to="/search">Search</Link>
                 <Link className="nav-link" to="/library">Library</Link>
